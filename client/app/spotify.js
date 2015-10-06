53be508b3df2ccba30a64ef3804294f117c1445f
brianpjackson88@gmail.com <brianpjackson88@gmail.com>
2015-10-06 14:52:03 -0600

    added flowRouter

--- client/app/spotify.js
+++ client/app/spotify.js
@@ -50,18 +50,6 @@ Template.spotify.helpers({
     selectedTracks.push(suggestion);
     Session.set('selectedTracks', selectedTracks);  // Session is only reactive on *set* being called.
   }
-
-  exampleMapOptions: function() {
-    // Make sure the maps API has loaded
-    if (GoogleMaps.loaded()) {
-      // Map initialization options
-      return {
-        center: new google.maps.LatLng(-37.8136, 144.9631),
-        zoom: 8
-      };
-    }
-  }
-});
 });
 
 Template.spotify.events({
