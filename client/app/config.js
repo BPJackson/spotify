0c46544634c69df009c3affc88bc20d909e66e3a
brianpjackson88@gmail.com <brianpjackson88@gmail.com>
2015-10-06 13:56:46 -0600

    added google maps

---
+++ client/app/config.js
@@ -0,0 +1,6 @@
+var scopes = ['playlist-modify-private', 'user-library-read','user-follow-read', 'playlist-read-private'];
+Accounts.ui.config({'requestPermissions':{'spotify':scopes}});
+
+Meteor.startup(function() {
+  GoogleMaps.load();
+});
